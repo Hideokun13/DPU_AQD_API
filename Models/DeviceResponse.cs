0d0ee47580e06e6448b4e7148d810f8856f3e349
Hideokun13 <77058528+Hideokun13@users.noreply.github.com>
2022-10-29 21:20:57 +0700

    Create DeviceResponse.cs

---
+++ Models/DeviceResponse.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DPU_AQD_API
+{
+    public class DeviceResponse
+    {
+        public string DeviceID {get; set;}
+        public string DeviceName {get; set;}
+        public string Isinstalled {get; set;}
+    }
+}
