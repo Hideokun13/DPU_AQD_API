498a0bbf63bb8aab68fa69c8b332fe7e571f44a9
Hideokun13 <77058528+Hideokun13@users.noreply.github.com>
2022-11-16 10:52:42 +0700

    change to new DB

--- Model/SQLConection.cs
+++ Model/SQLConection.cs
@@ -3,11 +3,11 @@ namespace DPU_AQD_API.Models;
 
 public class SQLConection
 {
-    private const string Endpoint = "dpu-aqd-db.cea8uizk3jzd.ap-southeast-1.rds.amazonaws.com";
-    private const string Port = "3306";
-    private const string DBUser = "admin";
-    private const string DBPwd = "admin1234!";
-    private const string DBName = "DPU_AQD_DB";
+    private const string Endpoint = "ibestdk.3bbddns.com";
+    private const string Port = "53886";
+    private const string DBUser = "aql_admin";
+    private const string DBPwd = "K7y%8l09j";
+    private const string DBName = "aql_db";
     private const string ConnectionOption = "Convert Zero Datetime=True";
 
     public string strConnection = "server=" + Endpoint + ";port=" + Port + ";user=" + DBUser + ";password=" + DBPwd + ";database=" + DBName + ";" + ConnectionOption;
