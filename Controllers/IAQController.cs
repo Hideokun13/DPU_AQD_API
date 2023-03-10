6bbddab5862d3471a6fcdff7a1b564def63e3e42
Hideokun13 <77058528+Hideokun13@users.noreply.github.com>
2023-03-10 14:04:49 +0700

    update 10/3/2023

--- Controllers/IAQController.cs
+++ Controllers/IAQController.cs
@@ -39,16 +39,162 @@ public class IAQController : ControllerBase
             return Ok(iAQResponses);
         }
     }
-    private void FindMaxEq(){
-        //
+    [HttpGet("CalculateIAQ")]
+    public async Task<IActionResult> CalculateIAQ()
+    {
+        double[] eqArr = new double [2];
+        using (MySqlConnection connection = new MySqlConnection(sQLConection.strConnection))
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "getAVG_Eq"; //Store Procedure Name
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            await connection.OpenAsync();
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<EqResponse> eqResponses = new List<EqResponse>();
+            while (reader.Read())
+            {
+                EqResponse eqResponse = new EqResponse();
+                eqResponse.avg_pm2_5 = Convert.ToDouble(reader["avg(PM2_5)"]);
+                eqResponse.avg_pm10 = Convert.ToDouble(reader["avg(PM_10)"]);
+                eqResponse.max_pm2_5 = Convert.ToInt32(reader["max(PM2_5)"]);
+                eqResponse.min_pm2_5 = Convert.ToInt32(reader["min(PM2_5)"]);
+                eqResponse.max_pm10 = Convert.ToInt32(reader["max(PM_10)"]);
+                eqResponse.min_pm10 = Convert.ToInt32(reader["min(PM_10)"]);
+
+                eqResponses.Add(eqResponse);
+
+                eqArr[0] = eqResponse.avg_pm2_5;
+                eqArr[1] = eqResponse.avg_pm10;
+            }
+            await connection.CloseAsync();
+            
+            //Calculation
+            int[] FindEqResult = FindMaxEq(eqArr);
+            int[] FindX_MaxMinResult = FindX_MaxMin(FindEqResult[0], FindEqResult[1]);
+            int[] FindIAQ_MaxMinResult = FindIAQ_MaxMin(FindEqResult[0], FindEqResult[1]);
+            
+            IAQResponse iAQResponse = new IAQResponse();
+            iAQResponse.IAQ_ID = 1;
+            iAQResponse.timestamp = DateTime.UtcNow;
+            iAQResponse.Value = IAQCalculation(FindEqResult[0], FindX_MaxMinResult[0], FindX_MaxMinResult[1], FindIAQ_MaxMinResult[0], FindIAQ_MaxMinResult[1]);
+            
+            return Ok(iAQResponse);
+        }
     }
-    private int FindIAQ_MaxMin(int x){
-        return x;
+    private int[] FindMaxEq(double[] input){
+        double maxEq = input[0];
+        int eqIndex = 0;
+        int[] result = new int[2];
+        for(int i = 0; i < input.Length; i++){
+            if(input[i] > maxEq){
+                maxEq = input[i];
+                eqIndex = i;
+            }
+        }
+        result[0] = Convert.ToInt32(maxEq);
+        result[1] = eqIndex;
+        
+        return result;
     }
-    private int FindX_MaxMin(int x){
-        return x;
+    private int[] FindIAQ_MaxMin(int x, int eqIndex){
+        int[] max_min_IAQ = new int[2];
+        if(eqIndex == 0){ //PM 2.5
+            if(x < 26){
+                max_min_IAQ[0] = 0; //Min value
+                max_min_IAQ[1] = 25; //Max value
+            }
+            else if(x < 38){
+                max_min_IAQ[0] = 26; //Min value
+                max_min_IAQ[1] = 50; //Max value
+            }
+            else if(x < 51){
+                max_min_IAQ[0] = 51; //Min value
+                max_min_IAQ[1] = 100; //Max value
+            }
+            else if(x < 91){
+                max_min_IAQ[0] = 100; //Min value
+                max_min_IAQ[1] = 200; //Max value
+            }
+            else {
+                max_min_IAQ[0] = 201; //Min value
+                max_min_IAQ[1] = 0; //Max value
+            }
+        }
+        else if(eqIndex == 1){ //PM 10
+            if(x < 51){
+                max_min_IAQ[0] = 0; //Min value
+                max_min_IAQ[1] = 25; //Max value
+            }
+            else if(x < 81){
+                max_min_IAQ[0] = 26; //Min value
+                max_min_IAQ[1] = 50; //Max value
+            }
+            else if(x < 121){
+                max_min_IAQ[0] = 51; //Min value
+                max_min_IAQ[1] = 100; //Max value
+            }
+            else if(x < 181){
+                max_min_IAQ[0] = 101; //Min value
+                max_min_IAQ[1] = 200; //Max value
+            }
+            else {
+                max_min_IAQ[0] = 201; //Min value
+                max_min_IAQ[1] = 0; //Max value
+            }
+        }
+        return max_min_IAQ;
     }
-    private int IAQCalculation(int x, int min_x, int max_x, int i, int min_i, int max_i){
-        return ((max_i - min_i)/(max_x - min_x) * (x - min_x)) + min_i;
+    private int[] FindX_MaxMin(int x, int eqIndex){
+        int[] max_min_Eq = new int[2];
+        if(eqIndex == 0){ //PM 2.5
+            if(x < 26){
+                max_min_Eq[0] = 0; //Min value
+                max_min_Eq[1] = 25; //Max value
+            }
+            else if(x < 38){
+                max_min_Eq[0] = 26; //Min value
+                max_min_Eq[1] = 37; //Max value
+            }
+            else if(x < 51){
+                max_min_Eq[0] = 38; //Min value
+                max_min_Eq[1] = 50; //Max value
+            }
+            else if(x < 91){
+                max_min_Eq[0] = 51; //Min value
+                max_min_Eq[1] = 90; //Max value
+            }
+            else {
+                max_min_Eq[0] = 91; //Min value
+                max_min_Eq[1] = 0; //Max value
+            }
+        }
+        else if(eqIndex == 1){ //PM 10
+            if(x < 51){
+                max_min_Eq[0] = 0; //Min value
+                max_min_Eq[1] = 50; //Max value
+            }
+            else if(x < 81){
+                max_min_Eq[0] = 51; //Min value
+                max_min_Eq[1] = 80; //Max value
+            }
+            else if(x < 121){
+                max_min_Eq[0] = 81; //Min value
+                max_min_Eq[1] = 120; //Max value
+            }
+            else if(x < 181){
+                max_min_Eq[0] = 121; //Min value
+                max_min_Eq[1] = 180; //Max value
+            }
+            else {
+                max_min_Eq[0] = 181; //Min value
+                max_min_Eq[1] = 0; //Max value
+            }
+        }
+        return max_min_Eq;
+    }
+    private int IAQCalculation(int x, int min_x, int max_x, int min_i, int max_i){
+        return (int)(((float)(max_i - min_i) / (max_x - min_x)) * (x - min_x) + min_i);
     }
 }
--- Model/IAQResponse.cs
+++ Model/IAQResponse.cs
@@ -6,10 +6,4 @@ public class IAQResponse
     public int IAQ_ID {get; set;}
     public DateTime timestamp {get; set;}
     public int Value {get; set;}
-    public int x {get; set;}
-    int min_x {get; set;}
-    int max_x {get; set;}
-    int i {get; set;}
-    int min_i {get; set;}
-    int max_i {get; set;}
 }
