081a7260b763032d04f8d39802f37a16e7ca76b2
Hideokun13 <boypeerawit@hotmail.com>
2023-07-15 20:42:37 +0700

    Create DeviceStatusEachDevice.cs

---
+++ Model/DeviceStatusEachDevice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DPU_AQD_API.Models;
+public class DeviceStatusEachDevice
+{
+    public int BuildingID { get; set; }
+    public string BuildingName { get; set; }
+    public int RoomID { get; set; }
+    public string RoomName { get; set; }
+    public int DeviceID { get; set; }
+    public string StatusID { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Sensor_Status { get; set; }
+
+}
