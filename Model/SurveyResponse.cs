701acf34102be8927fa9e742a34263ba55f79c37
Hideokun13 <boypeerawit@hotmail.com>
2023-07-16 16:57:27 +0700

    Create SurveyResponse.cs

---
+++ Model/SurveyResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace DPU_AQD_API.Models;
+
+public class SurveyResponse
+{
+    public string surveyID { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string SubmitData { get; set; }
+}
