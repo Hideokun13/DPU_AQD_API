08f3a5f97278ad9e6c20cc1dfdd847938347c4f1
Hideokun13 <boypeerawit@hotmail.com>
2023-07-17 13:20:17 +0700

    Update SurveyResponse.cs

--- Model/SurveyResponse.cs
+++ Model/SurveyResponse.cs
@@ -6,4 +6,6 @@ public class SurveyResponse
     public string surveyID { get; set; }
     public DateTime Timestamp { get; set; }
     public string SubmitData { get; set; }
+    public int BuildingID { get; set; }
+    public string RoomName { get; set; }
 }
