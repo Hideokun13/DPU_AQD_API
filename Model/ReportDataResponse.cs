6f0fbe97a80b7256430c553b81836d010c8e650e
Hideokun13 <boypeerawit@hotmail.com>
2023-07-18 11:18:25 +0700

    Update ReportDataResponse.cs

--- Model/ReportDataResponse.cs
+++ Model/ReportDataResponse.cs
@@ -5,7 +5,6 @@ public class ReportDataResponse
     public string Timestamp { get; set; }
     public int Temp { get; set; }
     public int Humidity { get; set; }
-    public int CO2 { get; set; }
     public int VOC { get; set; }
     public int PM2_5 { get; set; }
     public int PM_10 { get; set; }
